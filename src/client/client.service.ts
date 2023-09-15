c37a4070af1ff8641520dc4b15ee30f32ec0b20e
Alexis Cano <canoalexis12@gmail.com>
2023-09-14 20:34:20 -0600

    Version1

---
+++ src/client/client.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class ClientService {
+    private clients = [
+        {
+            id: 1,
+            nombre: 'cliente1',
+            direccion: 'direccion1'
+        },
+        {
+            id: 2,
+            nombre: 'cliente2',
+            direccion: 'direccion2'
+        }
+    ]
+
+    findAll() {
+        return this.clients;
+    }
+
+    findById(id: number) {
+
+        const client = this.clients.find(c => c.id === id);
+        if (!client) {
+            throw new NotFoundException(`Client with id ${id} does not exist`);
+        }
+        return client;
+    }
+
+    updateClient(clientBody, id) {
+        //Validaciones
+
+        const client = this.clients.find(c => c.id === id);
+
+        if (!client) {
+            throw new NotFoundException(`Client with id ${id} does not exist`);
+        }
+
+        //Logica para actualizar
+
+        return { status: 'cliente actualizado' };
+    }
+
+    deleteClient(id) {
+
+        const client = this.clients.find(c => c.id === id);
+
+        if (!client) {
+            throw new NotFoundException(`Client with id ${id} does not exist`);
+        }
+
+        //Logica para eliminar
+
+        return { status: 'cliente eliminado' };
+    }
+
+    createClient(clientBody) {
+        //Validaciones
+
+        const client = this.clients.find(c => c.id === clientBody.id);
+
+        if (client) {
+            throw new NotFoundException(`Client with id ${clientBody.id} already exist`);
+        }
+
+        //Logica para crear el cliente
+        return { status: 'cliente creado' };
+    }
+}
